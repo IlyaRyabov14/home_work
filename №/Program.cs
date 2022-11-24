00842a8dcc75e2b3e7bdbd097d04dd51e4ac889e
ilya <ilya-ryabov03@yandex.ru>
2022-11-25 00:20:27 +0300

    add_details

--- №/Program.cs
+++ №/Program.cs
@@ -2,7 +2,7 @@
 string [] result = new string [4];
 FullArray(text);
 SortArray(text, result);
-
+PrintArray(text,result);
 void FullArray(string [] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -19,10 +19,23 @@ void SortArray(string [] array, string [] helpArray)
         if (array[i].Length <= 3)
         {
             helpArray[i] = array[i];
-            Console.Write(helpArray[i] );
-
         }
     }
 }
 
+void PrintArray(string [] array, string [] helpArray)
+{
+    Console.Write("(");
+    for(int i = 0; i <array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(")-->");
+    Console.Write("(");
+    for(int i = 0; i <helpArray.Length; i++)
+    {
+        Console.Write(helpArray[i] + " ");
+    }
+    Console.Write(")");
+}
 
