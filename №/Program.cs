403ccc2fef4ce72e019a712fdf7cc9e61e28958c
ilya <ilya-ryabov03@yandex.ru>
2022-11-24 23:45:47 +0300

    add_text

---
+++ №/Program.cs
@@ -0,0 +1,11 @@
+﻿string [] text = new string [4];
+
+void FullArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите слово: ");
+        array[i] = Console.ReadLine() ?? "";
+    }
+}
+FullArray(text);
